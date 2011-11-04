cce4576a469e1edcfa4e418f8ac6bf8991a34af7
Justin Jones <justin@airbnb.com>
2011-11-03 17:19:53 -0700

    pass features and strategies to admin interface

--- app/controllers/trebuchet_controller.rb
+++ app/controllers/trebuchet_controller.rb
@@ -1,7 +1,11 @@
 class TrebuchetController < ::ApplicationController
 
   def index
-    # 
+    @features = {}
+    Trebuchet.backend.get_features.each do |f|
+      @features[f] = Trebuchet.backend.get_strategy(f)
+    end
+    # raise @features.inspect
   end
   
 end
