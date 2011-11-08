23f15ad6f2bbd7b4c71950d7f56d7485cabe6bfb
Justin Jones <justin@airbnb.com>
2011-11-08 13:40:25 -0800

    comment cruft

--- app/controllers/trebuchet_controller.rb
+++ app/controllers/trebuchet_controller.rb
@@ -2,7 +2,6 @@ class TrebuchetController < ::ApplicationController
 
   def index
     @features = Trebuchet::Feature.all
-    # @features.reject! {|f| f.valid? == false }
     @features.sort! {|x,y| x.name.downcase <=> y.name.downcase }
     
     respond_to do |wants|
