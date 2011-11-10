0767d42935cb479c3bd77b52b2e4ea62aa7cdd13
Justin Jones <justin@airbnb.com>
2011-11-09 16:18:18 -0800

    set namespace by default (needs work)

--- rails/init.rb
+++ rails/init.rb
@@ -1,11 +1,20 @@
 require 'trebuchet'
 
+
+# happens too late
+def set_trebuchet_namespace(app_name)
+  if Trebuchet.backend.respond_to?(:namespace=)
+    Trebuchet.backend.namespace = "trebuchet-#{app_name}/"
+  end
+end
+
 if defined? Rails
   Trebuchet.use_with_rails!
   
   if Rails.respond_to?(:version) && Rails.version =~ /^3/
     # Rails 3.x
     # use Railtie
+    # set_trebuchet_namespace "#{app_name}-#{Rails.env}"
   else
     # Rails 2.x
     config ||= Rails.configuration if Rails.respond_to?(:configuration)
@@ -13,5 +22,6 @@ if defined? Rails
     load_paths.each do |path|
        ActiveSupport::Dependencies.load_once_paths.delete(path)
     end if config.environment == 'development'
+    set_trebuchet_namespace "#{Rails.root.basename}-#{Rails.env}" 
   end
 end
