e2cc4b292e2cd118ad4dd36cd150c3ebd5c8ee72
Justin Jones <justin@airbnb.com>
2011-11-11 12:32:46 -0800

    fixed lingering array.flatten and cleaned up commented code

--- lib/trebuchet/backend/redis.rb
+++ lib/trebuchet/backend/redis.rb
@@ -11,12 +11,6 @@ class Trebuchet::Backend::Redis
   end
 
   def get_strategy(feature_name)
-    # if s = @redis.hget(key, feature_name)
-    #       s = JSON.load(s)
-    #       s.map! {|e| e && s.index(e).even? ? e.to_sym : e} # set strategy names to symbols
-    #     else
-    #       nil
-    #     end
     return nil unless h = @redis.hgetall(feature_key(feature_name))
     [].tap do |a|
       h.each do |k, v|
@@ -24,34 +18,23 @@ class Trebuchet::Backend::Redis
         a << JSON.load(v).first # unpack from array
       end
     end
-    h.map {|k,v| [k.to_sym, JSON.load(v).first]}.flatten
   end
 
   def set_strategy(feature_name, strategy, options = nil)
-    # @redis.hset(key, feature_name, [strategy, options].to_json)
-    
     @redis.del(feature_key(feature_name))
     append_strategy(feature_name, strategy, options)
-    
   end
 
   def append_strategy(feature_name, strategy, options = nil)
-    # s = get_strategy(feature_name) || []
-    # @redis.hset(key, feature_name, (s + [strategy, options]).to_json)
     @redis.hset(feature_key(feature_name), strategy, [options].to_json) # have to put options in container for json
     @redis.sadd(feature_names_key, feature_name)
   end
   
   def get_feature_names
-    # @redis.hkeys(key)
     @redis.smembers(feature_names_key)
   end
 
   private
-
-  # def key(feature_name)
-  #   '#{namespace}features/#{feature_name}'
-  # end
   
   def feature_names_key
     "#{namespace}feature-names"
@@ -59,14 +42,6 @@ class Trebuchet::Backend::Redis
   
   def feature_key(feature_name)
     '#{namespace}features/#{feature_name}'
-  end
-
-  def load_json(str)
-    begin
-      JSON.load(str)
-    rescue
-      nil
-    end
   end
 
 end
