db98fac07db7eab529f5b9951e2ac6e1651fc647
Justin Jones <justin@airbnb.com>
2011-11-15 17:32:12 -0800

    specfix

--- spec/redis_backend_spec.rb
+++ spec/redis_backend_spec.rb
@@ -12,17 +12,18 @@ describe Trebuchet::Backend::Redis do
   it "should set redis client if passed in" do
     r = Redis.new
     Redis.stub!(:new).and_return(nil)
-    Trebuchet.set_backend :redis
+    lambda {Trebuchet.set_backend :redis}.should raise_error Trebuchet::BackendInitializationError
     Trebuchet.backend.instance_variable_get(:@redis).should be_nil
     Trebuchet.set_backend :redis, :client => r
     Trebuchet.backend.instance_variable_get(:@redis).should eql r
   end
   
   it "should pass arguments to Redis.new" do
-    Redis.should_receive(:new).with(:this => 'that', :other => true)
+    r = Redis.new
+    Redis.should_receive(:new).with(:this => 'that', :other => true).and_return(r)
     Trebuchet.set_backend :redis, :this => 'that', :other => true
     args = [:seven, {8 => 'nine'}]
-    Redis.should_receive(:new).with(*args)
+    Redis.should_receive(:new).with(*args).and_return(r)
     Trebuchet.set_backend :redis, *args
   end
   
